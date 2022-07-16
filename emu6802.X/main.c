c0ae00eaced4193d2fdc69dc20fff93c73296324
ryu10 <ryu10nt@gmail.com>
2022-07-15 23:33:21 -0700

    first working code

--- emu6802.X/main.c
+++ emu6802.X/main.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include "PortAssign.h"
 #include "ReadAddress.h"
-//#include "RomRam.h"
+#include "RomRam.h"
 
 /*
  * main
@@ -66,22 +66,39 @@ int main(int argc, char** argv) {
     
     while(1) {
         if(MemAccess){
-            printf("VMA detected. Address= %04x %02x:%02x\r\n", ab.w, ab.h, ab.l);
+//            printf("VMA detected. Address= %04x %02x:%02x\r\n", ab.w, ab.h, ab.l);
             CLCSELECT = 1; // select CLC2=MRdy FF
-            MPU_MRDY = 1;
-            while(1){;}
+            MPU_MRDY = 0; // extend mem cycle phase
          
             // Switch R/W
             if(MPU_RW){
                 // MPU read = PIC Data bus output
                 MPU_DDIR = 0; // PIC Data bus output
-            // Switch mem acc range
-            // expose mem content to Port C
+                if(ab.w < RAM0_END){ // read ram
+                    LATC = ram0[ab.w - RAM0_BEG];
+                }else if(ab.w >= ROM0_BEG){ // read rom
+                    if(ab.w < ROM0_END){
+                        LATC = rom0[ab.w - ROM0_BEG];
+                    }else if(ab.w >= ROM1_BEG){
+                        LATC = rom1[ab.w - ROM1_BEG];
+                    }
+                    }else if(ab.w == UART_DREG){
+                    LATC = U3RXB;
+                }else if(ab.w == UART_CREG){
+                    LATC = PIR9;
+                }
                 MPU_DDIR = 0xff; // PIC Data bus input
             }else{
                 // MPU write = PIC Data bus input
             // Switch mem acc range
             // Read bus value into c and store ram/reg
+                while(MPU_E==0){;} // wait until the second half of MPU cycle
+                _delay(225*_XTAL_FREQ/1000000000); // 14 @ _XTAL_FREQ = 64000000, ~219ns
+                if(ab.w < RAM0_END){ // read ram
+                    ram0[ab.w - RAM0_BEG] = PORTC;
+                }else if(ab.w == UART_DREG){
+                    U3TXB = PORTC;
+                }
             }
             MemAccess = 0;
             // Clear Mem Stretch
